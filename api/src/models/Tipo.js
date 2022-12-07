ade1295db494a79fe44e937257f8b591a7207a12
David <cdvdrosero@gmail.com>
2022-12-06 19:53:06 -0500

    api request saved in db

--- api/src/models/Tipo.js
+++ api/src/models/Tipo.js
@@ -2,13 +2,13 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) =>{
     sequelize.define('Tipo',{
-        ID :{
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            allowNull: false,
-            primaryKey: true,
-        },
-        Nombre: {
+        // ID :{
+        //     type: DataTypes.INTEGER,
+        //     // defaultValue: DataTypes.UUIDV4,
+        //     // primaryKey: true,
+
+        // },
+        name: {
             type: DataTypes.STRING(20),
         }
     })
--- api/src/routes/typeRoute.js
+++ api/src/routes/typeRoute.js
@@ -1,12 +1,27 @@
 const { Router } = require("express");
-//modelo sacado de la database acá
+const { Tipo } = require("../db.js");
 const router = Router();
 
 router.get("/", (req, res) => {
+    // fetch("https://pokeapi.co/api/v2/type")
+    //   .then((response) => response.json())
+    //   .then((typeList) => res.status(200).send(typeList.results))
+    //   .catch((error) => console.log(error));
+
+    // Tipo.sync({force:true});
+
+  //este de abajo sirve pero le falta
+
   fetch("https://pokeapi.co/api/v2/type")
     .then((response) => response.json())
-    .then((typeList) => res.status(200).send(typeList))
+    .then((arr) => arr.results)
+    .then(data => Tipo.bulkCreate(data))
+    .then((data) => {Tipo.sync({alter:true})
+      return res.send(data);
+    })
     .catch((error) => console.log(error));
+
+
 });
 
 module.exports = router;
